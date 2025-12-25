f96c680baa8dc178c0b0181ff344e9da09fead14
firminkeambou <seven7digit2021@gmail.com>
2025-12-25 13:48:23 +0100

    clothes store with redux kit and serveless function2  added up to 194

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
-import { CardElement, useStripe,useElements } from '@stripe/react-stripe-js';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 import { PaymentFormContainer, FormContainer } from './payment-form.styles';
 
 import './payment-form.styles';
 const PaymentForm = () => {
-    const stripe = useStripe();
-    const elements = useElements();
-    const paymentHandler = async (e) => {
+  const stripe = useStripe();
+  const elements = useElements();
+  /* const paymentHandler = async (e) => {
     e.preventDefault();
     if (!stripe || !elements) {
       return;
     }
     //payment processing logic will go here}
-    };
+    };*/
   return (
     <PaymentFormContainer>
       <FormContainer>
